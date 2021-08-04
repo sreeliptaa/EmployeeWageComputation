b88e993085ca01010dc32c285fec316c127e8663
sreelipta <sreelipta65@gmail.com>
2021-08-05 01:56:34 +0530

    Refactored: calculate daily emp wage

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -3,12 +3,19 @@ package com.bridgelabz;
 public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        int IS_FULL_TIME = 1; //constant
-        //check employee attendance
+        //constant
+        int IS_PRESENT = 1;
+        int EMP_WAGE_PER_HOUR = 20;
+        int FULL_TIME = 8;
+        int calcWage = 0;
+        //calculation
         double empcheck = Math.floor(Math.random() * 10) % 2;
-        if (empcheck == IS_FULL_TIME)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
+        if (IS_PRESENT == empcheck) {
+            System.out.println("Employee is Present");
+            calcWage = FULL_TIME * EMP_WAGE_PER_HOUR;
+        } else {
+            System.out.println("Employee is Absent");
+        }
+        System.out.println("Employee  daily Wage = " + calcWage + " Rs ");
     }
 }
