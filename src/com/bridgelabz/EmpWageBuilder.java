9c0ed633b95b091b1a59ce57efd066dd85b1a6c2
sreelipta <sreelipta65@gmail.com>
2021-08-05 12:01:13 +0530

    Refactotr: calculating employee wage using switch case

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -1,34 +1,28 @@
 package com.bridgelabz;
 
 public class EmpWageBuilder {
+
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_WAGE_PER_HOUR = 20;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-
-        //constant
-        int EMP_WAGE_PER_HOUR = 20;
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-
         //variables
-        int calcWage = 0;
         int empHrs = 0;
-
-        //calculation
-        double empcheck = Math.floor(Math.random() * 10) % 3;
-
-        if (IS_FULL_TIME == empcheck)
-        {
-            empHrs = 8;
-            System.out.println("Employee is working as a Full-Time employee");  //Full time
+        int calcWage = 0;
+        // Computation
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
         }
-        else if (IS_PART_TIME == empcheck)
-        {
-            empHrs = 4;
-            System.out.println("Employee is working as a Part-Time employee ");  //Part time
-        }
-        else
-            empHrs = 0;
-
         calcWage = empHrs * EMP_WAGE_PER_HOUR;
         System.out.println("Employee Wage = " + calcWage + " Rs ");
     }
