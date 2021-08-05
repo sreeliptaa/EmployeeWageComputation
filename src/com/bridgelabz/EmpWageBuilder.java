5ea07f2d8e81190c2f27848c9c287a753ed5f262
sreelipta <sreelipta65@gmail.com>
2021-08-05 11:43:30 +0530

    Refactotr: part time employee and wage

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -3,19 +3,33 @@ package com.bridgelabz;
 public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+
         //constant
-        int IS_PRESENT = 1;
         int EMP_WAGE_PER_HOUR = 20;
-        int FULL_TIME = 8;
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+
+        //variables
         int calcWage = 0;
+        int empHrs = 0;
+
         //calculation
-        double empcheck = Math.floor(Math.random() * 10) % 2;
-        if (IS_PRESENT == empcheck) {
-            System.out.println("Employee is Present");
-            calcWage = FULL_TIME * EMP_WAGE_PER_HOUR;
-        } else {
-            System.out.println("Employee is Absent");
+        double empcheck = Math.floor(Math.random() * 10) % 3;
+
+        if (IS_FULL_TIME == empcheck)
+        {
+            empHrs = 8;
+            System.out.println("Employee is working as a Full-Time employee");  //Full time
         }
-        System.out.println("Employee  daily Wage = " + calcWage + " Rs ");
+        else if (IS_PART_TIME == empcheck)
+        {
+            empHrs = 4;
+            System.out.println("Employee is working as a Part-Time employee ");  //Part time
+        }
+        else
+            empHrs = 0;
+
+        calcWage = empHrs * EMP_WAGE_PER_HOUR;
+        System.out.println("Employee Wage = " + calcWage + " Rs ");
     }
 }
