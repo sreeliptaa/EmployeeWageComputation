2028ac8c91d1a6fa999c720e9dc33af46a3c9bb0
sreelipta <sreelipta65@gmail.com>
2021-08-05 12:33:38 +0530

    Refactotr: calculating employee wage for a month

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -5,25 +5,28 @@ public class EmpWageBuilder {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_WAGE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        //variables
-        int empHrs = 0;
-        int calcWage = 0;
+        int empHrs = 0, calcWage = 0, totalEmpWage =0;
         // Computation
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+        for (int day=0; day < NUM_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            calcWage = empHrs * EMP_WAGE_PER_HOUR;
+            totalEmpWage += calcWage;
+            System.out.println("Employee Daily Wage = " + calcWage + " Rs ");
         }
-        calcWage = empHrs * EMP_WAGE_PER_HOUR;
-        System.out.println("Employee Wage = " + calcWage + " Rs ");
+        System.out.println("Employee Wage for a Month = " + totalEmpWage + " Rs ");
     }
 }
