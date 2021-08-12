c3b7ff8007d27d1e82665479b17fafbc08321396
sreelipta <sreelipta65@gmail.com>
2021-08-12 16:04:32 +0530

    Added: computing employee wage of multiple companies

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -1,18 +1,13 @@
 package com.bridgelabz;
 
 public class EmpWageBuilder {
-
+     //constants
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    public static final int EMP_WAGE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int MAX_HRS_IN_MONTH = 100;
+    public static int computeEmpWage(String company, int EMP_WAGE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
 
-
-    public static int computeWage() {
         // Declaring the variables
         int empHrs = 0;
-        int totalEmpWage = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
 
@@ -35,14 +30,15 @@ public class EmpWageBuilder {
             totalEmpHrs += empHrs;
             System.out.println("Day " + totalWorkingDays + " : Employee worked : " + empHrs + " Hours ");
         }
-        totalEmpWage = totalEmpHrs * EMP_WAGE_PER_HOUR ;
+       int totalEmpWage = totalEmpHrs * EMP_WAGE_PER_HOUR ;
         System.out.println("Total Wages is : " + totalEmpWage);
         return totalEmpWage;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
-        computeWage();
+        computeEmpWage("Indiamart", 20 ,2 ,10);
+        computeEmpWage("Starmarket" ,10 ,4 ,20);
 
     }
 
